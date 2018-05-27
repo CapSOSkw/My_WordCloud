f3123d22357c6d5226a0dc9b15814fc07ed5f91e
KeyuanWu <wky0702@gmail.com>
2018-05-27 12:19:16 -0400

    Update mywc.py

--- mywc.py
+++ mywc.py
@@ -6,7 +6,7 @@ from wordcloud import WordCloud
 
 text = open(u'xinlin.txt', encoding='utf-8').read()
 
-alice_mask = np.array(Image.open('./examples/alice_mask.png'))
+alice_mask = np.array(Image.open('alice_mask.png'))
 font = 'SourceHanSansSC-Regular.otf'
 
 wc = WordCloud(background_color='white', max_words=2000, mask=alice_mask,
